b2bd7c869e61e7e547c0cd039115be2fed327d49
msventurini <matheus.venturini@inf.ufrgs.br>
2022-09-12 23:24:41 -0300

    Adding contents of the first day.

---
+++ Day-1/Part-4/examples.playground/Contents.swift
@@ -0,0 +1,21 @@
+import Cocoa
+import Foundation
+
+var greeting = "Hello, playground"
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphne"
+//character = "Eloise" - not allowed!
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let managerName = "Michael Scott"
+let dogBreed = "Samoyed"
+let meaningOfLife = "How many roads must a walk down?"
---
+++ Day-1/Part-7/examples.playground/Contents.swift
@@ -0,0 +1,18 @@
+import Cocoa
+
+let number = 0.1 + 0.2
+print(number)
+
+let a = 1
+let b = 2.0
+let c = Double(a) + b
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+
+var name = "Nicolas Cage"
+//name = 57 - not allowed!
+
+var rating = 5.0
+rating *= 2
